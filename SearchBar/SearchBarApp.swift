ee6d9c0a979155104963830b9838d888861a97de
Isaad Khan <IKhan@lcs.on.ca>
2023-04-17 14:17:49 -0400

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Isaad Khan on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
